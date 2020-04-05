ffc4f2932dd6d76c55e198355e02618ebdd154a3
Roy Xiong <rx28@duke.edu>
2020-04-05 14:36:34 -0400

    Started working on endpoint for schedules

--- flask app/app.py
+++ flask app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -20,6 +20,30 @@ def hello():
     print (courseoffs)
     return render_template('courseOff.html', courseoffs=courseoffs)
 
+@app.route('/schedule/<netid>/<schedule_number>', methods=['GET', 'POST'])
+def schedules(netid, schedule_number):
+	if request.method == 'GET':
+		courses = db.session.query(models.Schedule).filter(models.Schedule.netid == netid and models.Schedule.schedule_number == schedule_number).all()
+		j = []
+		for course in courses:
+			dict = {}
+			dict['subject'] = course.subject
+			dict['course_num'] = course.course_num
+			dict['type'] = course.type
+			dict['id'] = course.id
+			j.append(dict)
+		return jsonify(j)
+	elif request.method == 'POST':
+		if request.is_json:
+			data = request.get_json()
+			new_course = db.session.execute('INSERT INTO Schedule VALUES(:net_id, :schedule_num, :subject, :course_num, :course_type, :course_id', dict(net_id=netid, schedule_num=schedule_number, subject=data['subject'], course_num=data['course_num'], course_type=data['type'], course_id=data['id'])
+			print(new_course)
+			##TODO: see format of new_course and then figure out return value
+			return data
+		else:
+			return jsonify(status='Request was not JSON')
+
+	
 
 @app.route('/<subject>', methods=['GET'])
 def course_off_subject(subject):
