98139955ffbd058891c674023679873904a69bb2
jeremy.cohen <jeremy.cohen@duke.edu>
2020-04-25 18:37:16 -0700

    removed files

--- Flask App/dropme.sql
+++
@@ -1,6 +0,0 @@
-Drop table department cascade;
-Drop table class cascade;
-Drop table courseoff cascade;
-Drop table professor cascade;
-Drop table courseprof cascade;
-Drop table corequisite cascade;
